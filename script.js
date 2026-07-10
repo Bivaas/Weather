5f7443cf67ca14201397e9649cba8caea404bc46
Bivaas <bivaasbaral7@gmail.com>
2026-07-11 00:25:06 +0545

    API and express setup

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const cityInput = document.querySelector(".city-input");
 
 
 
-
 const getCityCoordinates = () => { 
 
     const cityName =  cityInput.value.trim();
+
+    if (!cityName) return;
+
+    const response = await fetch(`/api/geocode?city=${encodeURIComponent(cityName)}`);
+    const data = await response.json();
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("."));
+
+
+app.get("/api/geocode", async (req, res) => {
+
+    const city = req.query.city;
+
+    if (!city) { 
+        return res.status(400).json({ error: "Not a valid city !!"})
+    }
+
+    try { 
+        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=5&appid=${process.env.OPENWEATHER_API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        res.json(data);
+
+    } catch (error) {
+
+        res.status(500).json({ error: "geocoding fetch failed !!"});
+    }
+});
+
+app.listen(3000, () => {
+
+    console.log ("Server up and running !!")
+})
